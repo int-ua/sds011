ad42f19ed43a37bf9add7cf54c6aa9e934fc2194
Serhiy Zahoriya <serhiy.int@gmail.com>
2020-12-08 11:16:21 +0200

    pep8 console example

--- sds011/examples/sds011_console_test.py
+++ sds011/examples/sds011_console_test.py
@@ -1,15 +1,20 @@
+import csv
+
 from sds011 import SDS011
 
 port = "/dev/ttyUSB0"
 
-sds = SDS011(port=port,use_database=True)
-#sds.set_working_period(rate=5)#one measurment every 5 minutes offers decent granularity and at least a few years of lifetime to the sensor
+sds = SDS011(port=port, use_database=True)
+# one measurement every 5 minutes offers decent granularity
+# and at least a few years of lifetime to the sensor
+# sds.set_working_period(rate=5)
 print(sds)
-import csv
+
 try:
-    with open("measurments.csv","w") as csvfile:
-        log = csv.writer(csvfile, delimiter=" ",quotechar="|", quoting=csv.QUOTE_MINIMAL)
-        logcols = ["timestamp","pm2.5","pm10","device_id"]
+    with open("measurments.csv", "w") as csvfile:
+        log = csv.writer(
+            csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        logcols = ["timestamp", "pm2.5", "pm10", "device_id"]
         log.writerow(logcols)
         while True:
             meas = sds.read_measurement()
@@ -17,8 +22,7 @@ try:
             log.writerow(vals)
             csvfile.flush()
             print(vals)
-            
-            
+
 except KeyboardInterrupt:
-    #sds.sleep()
+    # sds.sleep()
     sds.__del__()

3b12da1140ecc0c91e938653c00d548ead04b7e1
Serhiy Zahoriya <serhiy.int@gmail.com>
2020-12-08 11:55:30 +0200

    pep8 utils

--- sds011/utils/sockethandler.py
+++ sds011/utils/sockethandler.py
@@ -1,20 +1,21 @@
-#file: sockethandler.py
-#author: (C) Patrick Menschel  2018
-#purpose: handle the socket communication of sds011 project
+# file: sockethandler.py
+# author: (C) Patrick Menschel  2018
+# purpose: handle the socket communication of sds011 project
 
-
-import threading
-from queue import Queue#,Empty
 import socket
+import threading
+
+from queue import Queue  # , Empty
+
 
 class sockethandler():
- 
-    def __init__(self,port=9999):
+
+    def __init__(self, port=9999):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "localhost"                           
+        self.host = "localhost"
         self.port = port
         self.sock.bind((self.host, self.port))
-        self.sock.listen(5)#up to 5 requests
+        self.sock.listen(5)  # up to 5 requests
         self.txqueue = Queue()
         self.listeners = []
         self.connhandler = threading.Thread(target=self.handle_connections)
@@ -23,28 +24,27 @@ class sockethandler():
         self.pushhandler = threading.Thread(target=self.pushoutmessage)
         self.pushhandler.setDaemon(True)
         self.pushhandler.start()
-        #print("init socket handler")
-         
- 
+        # print("init socket handler")
+
     def handle_connections(self):
-        #print("handles connections")
+        # print("handles connections")
         while True:
-            clientsocket,addr = self.sock.accept()
-            self.listeners.append((clientsocket,addr))
-     
+            clientsocket, addr = self.sock.accept()
+            self.listeners.append((clientsocket, addr))
+
     def pushoutmessage(self):
-        #print("pushing")
+        # print("pushing")
         while True:
             outmessage = self.txqueue.get()
-            msg = "{0}\n".format(",".join([str(outmessage.get(x)) for x in outmessage.keys()]))
-            #print(msg)
-            for clientsocket,addr in self.listeners:
+            msg = "{0}\n".format(
+                ",".join([str(outmessage.get(x)) for x in outmessage.keys()]))
+            # print(msg)
+            for clientsocket, addr in self.listeners:
                 clientsocket.sendall(msg.encode())
-             
-    def queue_tx_message(self,item):
+
+    def queue_tx_message(self, item):
         return self.txqueue.put(item=item)
-      
 
     def __del__(self):
-        #self.sock.shutdown(flag=)
+        # self.sock.shutdown(flag=)
         self.sock.close()
